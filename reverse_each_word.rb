82956f2e44003e1cc61230c34f8651a032d77ac5
DANIELLE PARK <dpark1008@gmail.com>
2018-11-27 05:07:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,10 +11,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
   array.collect do|string|
-    test_array << string.reverse 
+    test_array << string.reverse
   end
   test_array.join(" ")
 end
-
-
-reverse_each_word("Hello there, and how are you?")
